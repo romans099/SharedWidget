01f6dce3d67a99182174aa77ae5958699009c022
romans099 <romagupalowich2@gmail.com>
2026-09-20 16:20:03 +0300

    Add initial Android build configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,15 @@
+android {
+    namespace = "com.example.sharedwidget"
+
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.sharedwidget"
+
+        minSdk = 23
+        targetSdk = 35
+
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
